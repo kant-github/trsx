84b1400f8c6956efb10ae372fc8b2dca99430b40
Rishi Kant <khairrishi@Rishis-MacBook-Air.local>
2024-08-20 19:40:53 +0530

    Testing deploy.yaml

--- apps/user-app/components/AddMoneyCard.tsx
+++ apps/user-app/components/AddMoneyCard.tsx
@@ -14,6 +14,9 @@ const SUPPORTED_BANKS = [{
     redirectUrl: "https://www.axisbank.com/"
 }];
 
+
+
+
 export const AddMoney = () => {
     const [redirectUrl, setRedirectUrl] = useState(SUPPORTED_BANKS[0]?.redirectUrl);
     const [ amount, setAmount ] = useState(0);
